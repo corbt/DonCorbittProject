3da2c0fb962b30b4bc234f27736a3423cd094dc1
Kyle Corbitt <kyle@corbt.com>
2013-11-10 13:05:58 -0700

    working scraper

--- usenet/pull_site.js
+++ usenet/pull_site.js
@@ -1,2 +1,10 @@
-console.log('Hello, world!');
-phantom.exit();
+var args = require('system').args;
+
+var page = require('webpage').create();
+page.open(args[1], function () {
+    setTimeout(function() {
+    	console.log(page.content);
+    	phantom.exit();
+    }, 5000);
+    // return page.content;
+});
--- usenet/pull_threads.rb
+++ usenet/pull_threads.rb
@@ -1,1 +1,33 @@
+require 'fileutils'
+require 'pathname'
 require 'phantomjs'
+require './threads'
+
+class PullThreads
+  def self.pull thread_list
+    total = thread_list.size
+    thread_list.each_with_index.map do |thread, index|
+      puts "Downloading thread #{thread} (#{index+1}/#{total})"
+      list = thread.split('/')[-3]
+      id = thread.split('/')[-2..-1].join("-")
+
+      FileUtils.mkdir_p "raw/#{list}"
+      path = "raw/#{list}/#{id}.html"
+
+      html = nil
+      if Pathname.new(path).exist?
+        File.open(path) do |f|
+          html = f.read
+        end
+      else
+        html = Phantomjs.run("./pull_site.js", thread)
+        File.open(path, 'w') do |f|
+          f.write html
+        end
+      end
+      html
+    end
+  end
+end
+
+PullThreads.pull ThreadList::Threads
